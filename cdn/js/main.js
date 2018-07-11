5b4fdf52f44705bbe700c6330206d8f0c3db411b
yamen <yamen225@gmail.com>
2018-07-12 01:41:25 +0200

    single Page with add first dynamic card

--- cdn/js/main.js
+++ cdn/js/main.js
@@ -1,3 +1,5 @@
+var list;
+
 function Book (name, category, imgUrl, price, sPoints){
 	this.name = name;
 	this.category = category;
@@ -14,29 +16,8 @@ function Album (name, category, imgUrl, price, sPoints){
 	this.sPoints = sPoints;
 }
 
-let book1 = new Book("The Stranger", "Novel", "cdn/img/the-stranger.jpeg", 10, ["El Sherouq library", "Alef Library"]);
-let bookName1 = book1.name.split(' ').join();
-
-let book2 = new Book("A storm of Swords", "Novel", "cdn/img/got.jpg", 15, ["El Sherouq library", "Alef Library"]);
-let bookName2 = book2.name.split(' ').join();
-
-let Album1 = new Album("Best of Nirvana", "Alt-Rock", "cdn/img/Nirvana.png", 10, ["Itunes", "Spotify"]);
-
-let Album2 = new Album("Wish You Were Here", "Rock", "cdn/img/Pink.png", 15, ["Itunes", "Spotify"]);
-
-
-$(document).ready(() => {
-    $("#header1").load("header.html");
-    $("#footer").load("footer.html");
-
-    $('.nav-link').click(function(event){
-        event.preventDefault();
-    	let clicked_class = ($('#' + event.target.id).attr('class')).split(' ')[1];
-    	$('.data').removeClass('Active');
-    	$('.data').addClass('notActive');
-    	$('#'+clicked_class).toggleClass('Active notActive');
-    	if(clicked_class === 'books'){
-    		$('<div/>', {
+function createBook1(){
+	    	$('<div/>', {
     			'id':'books',
     			'class':'data Active',
     			}).appendTo($('.content'));
@@ -57,7 +38,7 @@ $(document).ready(() => {
     			}).appendTo($('#book1-card'));
 
     		$('<img/>', {
-    			'id':bookName1+'-imgUrl'
+    			'id':bookName1+'-imgUrl',
     			'class':'card-img-top',
     			'src':book1.imgUrl,
     			'alt':'book-cover',
@@ -70,17 +51,60 @@ $(document).ready(() => {
 
     		$('<div/>', {
     			'id':bookName1+'-text',
-    			'class':'book-name'
+    			'class':'book-name',
     		}).appendTo($('#'+bookName1+'-body'));
-
-    		$('#'+bookName1+'-body').append('<h5>'+book1.name+'</h5>');
+    		$('#'+bookName1+'-text').append('<h5>'+book1.name+'</h5>');
 
     		$('<div/>', {
     			'id':bookName1+'-category',
-    			'class':'book-category'
+    			'class':'book-category',
     		}).appendTo($('#'+bookName1+'-body'));
+    		$('#'+bookName1+'-category').append('<h6>'+book1.category+'</h6>'); 
 
-    		$('#'+bookName1+'-body').append('<h6>'+book1.category+'</h6>');    		
+    		$('<div/>', {
+    			'id':bookName1+'-sPoints',
+    			'class':'s-points',
+    		}).appendTo($('#'+bookName1+'-body'));
+    		$('#'+bookName1+'-sPoints').append('<p>Buy From: </p>');
+    		list = '<ul>';
+			for (var i in book1.sPoints) {
+    			list += '<li>'+i+'</li>';
+				}
+			list += '</ul>';
+			$('#'+bookName1+'-sPoints').append(list);
+
+    		$('<div/>', {
+    			'id':bookName1+'-footer',
+    			'class':'card-body',
+    		}).appendTo($('#'+bookName1));
+    		$('#'+bookName1+'-footer').append('<h4>Buy For:<span class="price">'+book1.price+'$</span></h4>');  		
+
+}
+
+let book1 = new Book("The Stranger", "Novel", "cdn/img/the-stranger.jpeg", 10, ["El Sherouq library", "Alef Library"]);
+let bookName1 = book1.name.split(' ').join('');
+
+let book2 = new Book("A storm of Swords", "Novel", "cdn/img/got.jpg", 15, ["El Sherouq library", "Alef Library"]);
+let bookName2 = book2.name.split(' ').join();
+
+let Album1 = new Album("Best of Nirvana", "Alt-Rock", "cdn/img/Nirvana.png", 10, ["Itunes", "Spotify"]);
+
+let Album2 = new Album("Wish You Were Here", "Rock", "cdn/img/Pink.png", 15, ["Itunes", "Spotify"]);
+
+
+$(document).ready(() => {
+    $("#header1").load("header.html");
+    $("#footer").load("footer.html");
+
+    $('.nav-link').click(function(event){
+        event.preventDefault();
+    	let clicked_class = ($('#' + event.target.id).attr('class')).split(' ')[1];
+    	$('.data').removeClass('Active');
+    	$('.data').addClass('notActive');
+    	$('#'+clicked_class).toggleClass('Active notActive');
+    	if(clicked_class === 'books'){
+    		$(".content").html("");
+    		createBook1();
     	}
     });
 
