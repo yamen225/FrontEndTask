6e83e58bbc2d0b563a1c1805f2ddf517992daeec
yamen <yamen225@gmail.com>
2018-07-12 02:28:38 +0200

    single Page with add books card dynamically using add function

--- cdn/js/main.js
+++ cdn/js/main.js
@@ -16,31 +16,35 @@ function Album (name, category, imgUrl, price, sPoints){
 	this.sPoints = sPoints;
 }
 
-function createBook1(){
-	    	$('<div/>', {
-    			'id':'books',
-    			'class':'data Active',
-    			}).appendTo($('.content'));
+function createBookRow(){
+	$('<div/>', {
+    	'id':'books',
+    	'class':'data Active',
+    }).appendTo($('.content'));
 
-    		$('<div/>', {
-    			'id':'book-row',
-    			'class':'row text-center',
-    			}).appendTo($('#books'));    		
+    $('<div/>', {
+    	'id':'book-row',
+    	'class':'row text-center',
+    }).appendTo($('#books'));  
+}
 
+function createBook(book){
+	let bookName1 = book.name.split(' ').join('');  		
+	alert(bookName1);
     		$('<div/>', {
-    			'id':'book1-card',
+    			'id':bookName1+'-card',
     			'class':'col-lg-3 col-md-6 mb-4',
     			}).appendTo($('#book-row'));
 
     		$('<div/>', {
     			'id':bookName1,
     			'class':'book Product',
-    			}).appendTo($('#book1-card'));
+    			}).appendTo($('#'+bookName1+'-card'));
 
     		$('<img/>', {
     			'id':bookName1+'-imgUrl',
     			'class':'card-img-top',
-    			'src':book1.imgUrl,
+    			'src':book.imgUrl,
     			'alt':'book-cover',
     			}).appendTo($('#'+bookName1));
 
@@ -53,13 +57,13 @@ function createBook1(){
     			'id':bookName1+'-text',
     			'class':'book-name',
     		}).appendTo($('#'+bookName1+'-body'));
-    		$('#'+bookName1+'-text').append('<h5>'+book1.name+'</h5>');
+    		$('#'+bookName1+'-text').append('<h5>'+book.name+'</h5>');
 
     		$('<div/>', {
     			'id':bookName1+'-category',
     			'class':'book-category',
     		}).appendTo($('#'+bookName1+'-body'));
-    		$('#'+bookName1+'-category').append('<h6>'+book1.category+'</h6>'); 
+    		$('#'+bookName1+'-category').append('<h6>'+book.category+'</h6>'); 
 
     		$('<div/>', {
     			'id':bookName1+'-sPoints',
@@ -67,8 +71,8 @@ function createBook1(){
     		}).appendTo($('#'+bookName1+'-body'));
     		$('#'+bookName1+'-sPoints').append('<p>Buy From: </p>');
     		list = '<ul>';
-			for (var i in book1.sPoints) {
-    			list += '<li>'+i+'</li>';
+			for (var i in book.sPoints) {
+    			list += '<li>'+book.sPoints[i]+'</li>';
 				}
 			list += '</ul>';
 			$('#'+bookName1+'-sPoints').append(list);
@@ -77,15 +81,13 @@ function createBook1(){
     			'id':bookName1+'-footer',
     			'class':'card-body',
     		}).appendTo($('#'+bookName1));
-    		$('#'+bookName1+'-footer').append('<h4>Buy For:<span class="price">'+book1.price+'$</span></h4>');  		
+    		$('#'+bookName1+'-footer').append('<h4>Buy For:<span class="price">'+book.price+'$</span></h4>');  		
 
 }
 
 let book1 = new Book("The Stranger", "Novel", "cdn/img/the-stranger.jpeg", 10, ["El Sherouq library", "Alef Library"]);
-let bookName1 = book1.name.split(' ').join('');
 
 let book2 = new Book("A storm of Swords", "Novel", "cdn/img/got.jpg", 15, ["El Sherouq library", "Alef Library"]);
-let bookName2 = book2.name.split(' ').join();
 
 let Album1 = new Album("Best of Nirvana", "Alt-Rock", "cdn/img/Nirvana.png", 10, ["Itunes", "Spotify"]);
 
@@ -104,7 +106,9 @@ $(document).ready(() => {
     	$('#'+clicked_class).toggleClass('Active notActive');
     	if(clicked_class === 'books'){
     		$(".content").html("");
-    		createBook1();
+    		createBookRow();
+    		createBook(book1);
+    		createBook(book2);
     	}
     });
 
